9c681cb95ef826e23579eaaff17bd5db1052c2d0
Urosh Trifunovic <nediarovic@gmail.com>
2018-09-19 00:07:38 +0200

    Add product-service project (WIP)

---
+++ product-service/src/test/java/com/adidas/demo/productservice/controller/ProductServiceControllerTests.java
@@ -0,0 +1,11 @@
+package com.adidas.demo.productservice.controller;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class ProductServiceControllerTests {
+
+}
